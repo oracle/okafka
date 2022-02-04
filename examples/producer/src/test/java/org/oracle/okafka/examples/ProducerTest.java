2942cdf263d3e8dd4300a1bf9b70bdfdbc8e65e5
Paulo Simoes <paulo.simoes@oracle.com>
2022-02-04 08:25:22 -0300

    WIP change examples project structure and create mvn prj.

---
+++ examples/producer/src/test/java/org/oracle/okafka/examples/ProducerTest.java
@@ -0,0 +1,20 @@
+package org.oracle.okafka.examples;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class ProducerTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
